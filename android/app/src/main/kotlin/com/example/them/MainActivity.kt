16b262cb289a17e908c60554be2370c5bb0ae1af
Nizam <ramazanov570633@gmail.com>
2024-01-21 15:22:50 +0400

    Theme and Utils model

---
+++ android/app/src/main/kotlin/com/example/them/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.them
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Themes/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Themeclass {
+  static ThemeData dark =
+      ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.red);
+  static ThemeData light =
+      ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.blue);
+}
